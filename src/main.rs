eab9d7d80934df99006f86c6a9122f5073bf67c1
Chai Phonbopit <phonbopit@gmail.com>
2022-04-13 00:12:02 +0700

    finish basic getting started

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,19 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+struct GreetTimer(Timer);
+
 fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Chuck Norris".to_string()));
     commands.spawn().insert(Person).insert(Name("John Doe".to_string()));
     commands.spawn().insert(Person).insert(Name("Prayuzz".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}", name.0);
+        }
     }
 }
 
@@ -34,8 +38,8 @@ impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         println!("HelloPlugin!");
 
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
 }
